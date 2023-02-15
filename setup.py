777ab9a0f748eb5a8fc1e426a1ca1da4ff7e66dd
CarlosLopezGit <47481629+CarlosLopezGit@users.noreply.github.com>
2023-02-15 14:46:00 -0500

    i pasted code #12

--- setup.py
+++ setup.py
@@ -1,1 +1,11 @@
-#tbd
+
+from setuptools import setup
+
+setup(name='virtualhat',
+      version='1.0',
+      description='CompE Stub Library',
+      author='Fraida Fund',
+      author_email='ffund@nyu.edu',
+      url='https://github.com/ffund/virtualhat',
+      py_modules=['virtualhat'],
+     )
