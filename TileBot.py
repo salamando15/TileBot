7a558b628027829c3bd85e8d36648f6de2a2e768
CarlosLopezGit <47481629+CarlosLopezGit@users.noreply.github.com>
2023-02-20 18:10:22 -0500

    added sections for different part functions, see #13

--- TileBot.py
+++ TileBot.py
@@ -1,6 +1,17 @@
 def info():  
     '''Prints a basic library description'''
     print("Software library for the TileBot project.")
-    
+#Camera Functions
 def cameraCapture():
     pass
+def readTiles():
+    pass
+def updateTileState():
+    pass
+#Color Sensor Functions
+
+#Add color sensor functions here
+
+#Robot Movement Functions
+
+#Add Robot Movement Function here
