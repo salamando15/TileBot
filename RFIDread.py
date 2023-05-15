110b97eb7a5e27223c686737f49592c2de6d1342
Zoom9000 <43012108+Zoom9000@users.noreply.github.com>
2023-05-15 14:09:40 -0400

    Add files via upload
    Read and Write Operations for RFID Reader

---
+++ RFIDread.py
@@ -0,0 +1,66 @@
+## Read Code
+
+#!/usr/bin/env python
+
+import RPi.GPIO as GPIO
+from mfrc522 import SimpleMFRC522
+import time
+import sys
+
+## A total of 10 cards will be used
+## Card for Hospital, School, Park, shopping center, /pier/resort/beach area, stadium, amusement park, a choice of food chain, supermarket,
+## a choice of land(mountains, lake, rural/farms, street, bridge, monument)
+def write():
+        print("Place and leave your tag on top of the reader to scan it ")
+        text = input("Enter new data to write to the tag, then click Enter: ")
+        reader.write(text)
+        print("Uploading ...")
+        time.sleep(2)
+        print("Success! Please remove your tag. ")
+        time.sleep(2)
+def scan():
+        print("Please hold a tag near the reader")
+        print("Reading tag in 3 seconds...")
+        ID, TXT= reader.read()
+        #print("HEXID: %s\nID: %s\nText: %s \n" % (ID,TXT))
+        return ID,TXT
+
+def resetIDs():
+    print("Please have all cards on you ready to be reset")
+    ID, TXT = scan()
+    time.sleep(1)
+    reader.write("")
+    time.sleep(1)
+
+## Create a dictionary that saves the RFID cards that come in the package
+## Each card has a unique ID
+keys = {}
+yes_choices = ['yes', 'y']
+no_choices = ['no', 'n']
+reader = SimpleMFRC522()
+
+4
+try:
+    while True:
+        id, text = scan()
+        time.sleep(2)
+        if (text==""):
+            print("The card scanned --> HEXID: %s\nID: %s\nText: %s \n" % (hex(id),id,text))
+            keys.update({id:text})
+            print(keys)
+            continue
+        else:
+            print("The card scanned --> HEXID: %s\nID: %s\nText: %s \n" % (hex(id),id,text))
+            change = input("Would you like to add/rewrite this card? Please enter yes or no: ")
+            if change.lower() in yes_choices:
+                write()
+                continue
+            elif change.lower() in no_choices:
+                print("Please remove the card from the reader")
+                continue
+            else:
+                print('Please type yes or no')
+                continue
+
+except KeyboardInterrupt:
+        GPIO.cleanup()
